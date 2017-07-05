4467e4106bc30267d08e40738b614c6b62b9b02c
Matheus Moscardini <matheus_moscardini@yahoo.com.br>
2017-07-05 18:32:50 -0300

    Inclusão bootstrap confirmada. Adaptação Home

--- wp-content/themes/allegiant-child/functions.php
+++ wp-content/themes/allegiant-child/functions.php
@@ -14,4 +14,16 @@ if ( get_stylesheet() !== get_template() ) {
     add_filter( 'pre_option_theme_mods_' . get_stylesheet(), function ( $default ) {
         return get_option( 'theme_mods_' . get_template(), $default );
     } );
-}?>
+}
+
+function add_meta_tags() {
+    echo '<meta property="og:title" content="Mini Maker - Kits de brinquedos mão na massa" />';
+    echo '<meta property="og:description" content="Novas formas de aprender com base nos conceitos do movimento maker. Kits para construção de brinquedos educativos criativos" />';
+    echo '<meta name="keywords" content="Movimento Maker, Educativos, Educação, aprendizagem, crianças, brincadeiras, brinquedos, criatividade" />';
+    echo '<meta property="og:type" content="website" />';
+    echo '<meta property="og:site_name" content="Mini Maker" />';
+    echo '<meta property="og:image" content="../../uploads/2017/07/logo-vermelho.png" />';
+}
+add_action('wp_head', 'add_meta_tags');
+
+?>
