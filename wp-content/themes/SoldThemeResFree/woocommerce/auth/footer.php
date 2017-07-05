375ac3490be8d061144cc92095d3a6fab105f012
Matheus Moscardini <matheus_moscardini@yahoo.com.br>
2017-07-04 22:26:04 -0300

    ativar plugins, inicio da configuração da loja

---
+++ wp-content/themes/SoldThemeResFree/woocommerce/auth/footer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Auth footer
+ *
+ * @author  WooThemes
+ * @package WooCommerce/Templates/Auth
+
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+?>
+		</div>
+	</body>
+</html>
